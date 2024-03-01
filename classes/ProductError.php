dbf96349d591f8e585d6c26cb4a782d3cdf3adbe
ThomasDumoulin <tymatho@bbox.fr>
2024-03-01 12:08:35 +0100

    Init project

---
+++ classes/ProductError.php
@@ -0,0 +1,11 @@
+<?php
+
+class ProductError
+{
+    public const NAME_REQUIRED = 1;
+    public const PARAMETERS_REQUIRED = 2;
+    public const CATEGORY_ID_REQUIRED = 3;
+    // public const DUPLICATE = 2;
+    // public const NAME_TOO_LONG = 3;
+    // ...
+}
