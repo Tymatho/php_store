24d5775aef32a2e38d3b25f082377484cc3022a5
ThomasDumoulin <tymatho@bbox.fr>
2024-03-08 16:10:43 +0100

    Add navbar to navigate easily

--- index.php
+++ index.php
@@ -5,14 +5,4 @@ require_once __DIR__ . '/layout/header.php';
 
 <h1>Bienvenue</h1>
 
-<nav>
-    <ul>
-        <li><a href="/">Accueil</a></li>
-        <li><a href="./page/categories.php">Catégories</a></li>
-        <li><a href="./page/products.php">Produits</a></li>
-        <li><a href="./page/add-category.php">Nouvelle catégorie</a></li>
-        <li><a href="./page/add-product.php">Nouveau produit</a></li>
-    </ul>
-</nav>
-
 <?php require_once __DIR__ . '/layout/footer.php'; ?>
